5997580f0f61f489b31cd0e0a5e3942a5842fda8
de337 <de337@TUFDANNY15>
2022-11-25 19:19:13 -0600

    hola

--- src/main/java/com/mycompany/video/Video.java
+++ src/main/java/com/mycompany/video/Video.java
@@ -14,5 +14,8 @@ public class Video {
     public static void main(String[] args) {
         System.out.println("DANIEL ");
         System.out.println(" ERICK");
+        
+        System.out.println("HERBER ");
+        System.out.println("ALEX");
     }
 }
